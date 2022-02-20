de89b9dd6c75ac5675a8ad26b16c997df0848062
Andrew <jkj43483@gmail.com>
2022-02-20 18:09:36 +0300

    HomeWork2

--- HomeWork2.java
+++ HomeWork2.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class HomeWork2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите свое имя: ");
+        String userName = scanner.next();
+        System.out.println("Hello: " + userName);
     }
 }
